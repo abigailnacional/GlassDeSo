0d067680867dec936123b3dd61d3f4b6ad636105
Abigail Nacional <aan16@duke.edu>
2022-03-19 00:25:24 -0400

    Got rid of some commented out code, made a page for users to create a new post

--- BitcloutAPI.py
+++ BitcloutAPI.py
@@ -2,20 +2,6 @@ import requests
 import json
 
 endpoint = 'https://api.bitclout.com'
-
-#def getUserHodlings(publicKey, username, checkForKey=None):
- #   # method gets holdings for publickey
-  #  data = { 'PublicKeyBase58Check':publicKey, 'Username':username, 'FetchAll':True}
-   # response = requests.post(endpoint+"/get-hodlers-for-public-key", json=data)
-    #data = json.loads(response.text)
-#    if(checkForKey != None ):
- #       for hodler in data['Hodlers']:
-  #          k = hodler['HODLerPublicKeyBase58Check']
-   #         print(k)
-    #        if(k == checkForKey):
-     #           return True
-      #  return False
-#    return data
         
 def getUserInfo(publicKey):
     #method gets all the details for the publickey
--- app.py
+++ app.py
@@ -1,30 +1,54 @@
+import requests
 from typing import Optional
 from xmlrpc.client import boolean
 from flask import Flask, flash, redirect, render_template, request, \
     url_for, session  # pragma: no cover
 from BitcloutIdentity import BitcloutIdentity
 import BitcloutAPI
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) 
 app.secret_key = "super secret key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
 
-#Class for posts made for GlassDeSo
-class GDSPosts:
-    def __init__(
-            self,
-            postID: int,
-            author: str,
-            company: str,
-            job: str,
-            offerStat: bool,
-            #hashVal is optional because we don't know what the hash is when we
-            #  post it; we have to look it up and add it later
-            hashVal: Optional[str] = ""):
-        self.postID = postID
+#Function that grabs the most recent post made using a DeSo username and post hash
+#The DeSo API automatically grabs the most recent post because I haven't provided a LastPostHashHex.
+def get_recent_post(username):
+ url = 'https://bitclout.com/api/v0/get-posts-for-public-key'
+ data = {
+   "PublicKeyBase58Check": "",
+   "Username": username,
+   "ReaderPublicKeyBase58Check": "",
+   "NumToFetch": 1,
+   "MediaRequired": False
+ }
+ response = requests.post(url,  json=data)
+ response_json = response.json()
+
+ #Get only the message content
+ ret = response_json['Posts'][0]['Body']
+
+ return ret
+
+#Class for posts made for GlassDeSo, offer_stat is offer status. Either they 
+# received an offer or they didn't. The post_ID is to keep track of posts in
+# this SQLAlchemy database, while the hashVal keeps track of what post it is
+# in the actual DeSo blockchain.
+class GDSPosts(db.Model):
+    post_ID = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(100))
+    company = db.Column(db.String(100))
+    job = db.Column(db.String(100))
+    offer_stat = db.Column(db.Boolean)
+    hashVal = db.Column(db.String(100), nullable=True)
+
+    def __init__(self, post_ID, author, company, job, offer_stat, hashVal: Optional[str] = ""):
+        self.post_ID = post_ID
         self.author = author
         self.company = company
         self.job = job
-        self.offerStat = offerStat
+        self.offer_stat = offer_stat
         self.hashVal = hashVal
 
 @app.route("/")
@@ -48,7 +72,6 @@ def login():
             if(not identity.validateJWT(jwt)):
                 error = "Could not login"
             else:
-                print('user holds NeonStoic. Log them in!')
                 session['pubkey'] = pubkey
                 return redirect('/home')
 
@@ -63,8 +86,19 @@ def home():
             session['userinfo'] = userinfo
             print('retrieved userinfo:', userinfo)
 
+            #I'm going to print the posts for a random user
+            get_recent_post("Krassenstein")
         return render_template('home.html', pubkey=session['pubkey'], userinfo=session['userinfo'])
     return redirect('/login')
+
+#Page where user creates a new post
+@app.route('/newpost')
+def newpost():
+    #If the form was just submitted:
+    #if request.method == 'POST':
+        #return render_template('postSuccess.html')
+    #else:
+        return render_template('newPost.html')
 
 @app.route('/logout')
 def logout():
